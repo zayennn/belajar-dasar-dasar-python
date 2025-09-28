1b3decb902fc01be8d3459ff6a81de97abef18ac
zayennn <zaayeennn@gmail.com>
2025-09-28 23:06:09 +0700

    upload pelajaran

---
+++ pelajaran 1/app.py
@@ -0,0 +1,7 @@
+a = 10
+# a itu adalah variabel yang menyimpan nilai 10 / variable adalah tempat menyimpan data
+# = adalah operator penugasan ( assignment operator )
+# 10 adalah nilai yang disimpan dalam variabel a ( value )
+
+# assignment indirect
+b = a
