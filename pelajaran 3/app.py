905bd1b73d9e89efeb240bf8d2a825309b47c582
zayennn <zaayeennn@gmail.com>
2025-09-28 23:18:41 +0700

    update materi

--- pelajaran 3/app.py
+++ pelajaran 3/app.py
@@ -1,30 +1,9 @@
-# tipe data: angka satuan yang ga ada koma nya ( integer )
-data_integer = 1
-print("data : ", data_integer)
-print("- bertipe : ", type(data_integer))
+# belajar casting
+# merubah dari satu tipe ke tipe lain
+# tipe data = int, float, str, bool
 
-# tipe data: angka dengan koma ( float )
-data_float = 1.5
-print("data : ", data_float)
-print("- bertipe : ", type(data_float))
+data_int = 9
+print("data : ", data_int, "type : ", type(data_int))
 
-# tipe data: kumpulan karakter ( string )
-data_string = "ucup"
-print("data : ", data_string)
-print("- bertipe : ", type(data_string))
-
-# tipwe data: biner true/false ( boolean )
-data_bool = True
-print("data : ", data_bool)
-print("- bertipe : ", type(data_bool))
-
-# tipe data khusus
-# bilangan kompleks
-data_complex = complex(5, 6)
-print("data : ", data_complex)
-print("- bertipe : ", type(data_complex))
-
-# tipe data dari bahasa C
-from ctypes import c_double
-data_c_double = c_double(10.5)
-print("data : ", data_c_double)
+data_float = float(data_int)
+print("data : ", data_float, "type : ", type(data_float))
