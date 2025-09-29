01be2605a9cfc07cbb306e8bdebe848a85e10743
zayennn <zaayeennn@gmail.com>
2025-09-29 11:08:44 +0700

    upload materi pelajaran 5

---
+++ pelajaran 5/app.py
@@ -0,0 +1,32 @@
+# operasi artimatika
+
+a = 10
+b = 3
+
+# penjumlahan
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# pengurangan
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# perkalian
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+# pembagian
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# pembagian bulat ( floor division )
+hasil = a // b
+print(a, "//", b, "=", hasil)
+
+# sisa bagi ( modulus )
+hasil = a % b
+print(a, "%", b, "=", hasil)
+
+# pangkat
+hasil = a ** b
+print(a, "**", b, "=", hasil)
