7bd90291f176e6db862582ace72f0d9762c6c55f
zayennn <zaayeennn@gmail.com>
2025-09-30 18:20:46 +0700

    upload materi pelajaran 8

--- pelajaran 8/app.py
+++ pelajaran 8/app.py
@@ -1,7 +1,40 @@
 # operasi logika atau boolean
 # not, or, and, xor
 
+
+# NOT ( kebalikan )
 print("==== NOT ====")
 
 a = True
 b = not a
+
+print('data a =', a)
+print('============NOT')
+print('data b =', b)
+
+
+
+# OR ( jika salah satu True, maka hasilnya True )
+print("\n==== OR ====")
+a = True
+b = False
+c = a or b
+print(a, 'OR', b, '=', c)
+
+
+
+# AND ( jika kedua-duanya True, maka hasilnya True )
+print("\n==== AND ====")
+a = True
+b = True
+c = a and b
+print(a, 'AND', b, '=', c)
+
+
+
+# XOR ( jika salah satu True, maka hasilnya True, jika kedua-duanya sama-sama True atau False, maka hasilnya False/True  )
+print("\n==== XOR ====")
+a = True
+b = False
+c = a ^ b
+print(a, 'XOR', b, '=', c)
